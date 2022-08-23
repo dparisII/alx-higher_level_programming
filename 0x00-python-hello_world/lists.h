914763654399bf64535841210a6af3be8e0bad03
dagmawi <dagmawiparis2016@gmail.com>
2022-08-23 13:22:12 -0700

    this is the lists.h file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,7 +1,17 @@
-#ifndef PYFILE
-#define PYFILE
+#ifndef LISTS_H
+#define LISTS_H
 
-0-run
+#include <stdlib.h>
 
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+}listint_t;
+
+size_t print_listint(const listint_t *h);
+listint *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
 #endif
